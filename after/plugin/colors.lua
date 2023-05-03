ae9f66369c7d2f49095be1a3bf5039f6ae86a548
activeagle <activespin7@gmai.com>
2023-05-03 21:53:11 +0700

    [+] modified plugin/

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,15 +1,46 @@
+local present, catppuccin = pcall(require, "catppuccin")
+if not present then return end
 
-require('rose-pine').setup({
-    disable_background = true
-})
+vim.opt.termguicolors = true
 
-function ColorMyPencils(color) 
-	color = color or "rose-pine"
-	vim.cmd.colorscheme(color)
+catppuccin.setup {
+	flavour = "mocha",
+	term_colors = true,
+	transparent_background = true,
+	no_italic = false,
+	no_bold = false,
+	styles = {
+		comments = { "italic" },
+		conditionals = {},
+		loops = {},
+		functions = { "italic" },
+		keywords = {},
+		strings = {},
+		variables = {},
+		numbers = {},
+		booleans = {},
+		properties = {},
+		types = { "bold" },
+	},
+	color_overrides = {
+		mocha = {
+			base = "#171717", -- background
+      surface2 = "#9A9A9A", -- comments
+      text = "#F6F6F6",
+		},
+	},
+	highlight_overrides = {
+		mocha = function(C)
+			return {
+				NvimTreeNormal = { bg = C.none },
+				CmpBorder = { fg = C.surface2 },
+				Pmenu = { bg = C.none },
+				NormalFloat = { bg = C.none },
+				TelescopeBorder = { link = "FloatBorder" },
+			}
+		end,
+	},
+}
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.cmd.colorscheme "catppuccin"
 
-end
-
-ColorMyPencils()
